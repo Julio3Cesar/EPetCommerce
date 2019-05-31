ebcee886a6adc674220127495d62fbe704a16721
Julio Cesar <julio.cesar@locaweb.com.br>
2019-05-30 21:35:29 -0300

    Add create account activity

--- app/src/main/java/com/example/epetcommerce/LoginActivity.java
+++ app/src/main/java/com/example/epetcommerce/LoginActivity.java
@@ -1,13 +1,18 @@
 package com.example.epetcommerce;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button btnCreateAccount;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +20,24 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        btnCreateAccount = findViewById(R.id.btnCreateAccount);
+        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent createAccontIntent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+                startActivity(createAccontIntent);
+            }
+        });
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(LoginActivity.this, bla.class);
+                startActivity(loginIntent);
+            }
+        });
     }
 
 }
