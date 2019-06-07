346eb6b8bf008ac8d40912f6a66c66c42681b90a
Julio Cesar <julio.cesar@locaweb.com.br>
2019-06-07 13:18:58 -0300

    Add payment Fragment

--- app/src/main/java/com/example/epetcommerce/PaymentFragment.java
+++ app/src/main/java/com/example/epetcommerce/PaymentFragment.java
@@ -6,24 +6,54 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PaymentFragment extends Fragment {
-
-
-    public PaymentFragment() {
-        // Required empty public constructor
-    }
-
+    private Button btnFinalize;
+    private ImageView imgCreditCard;
+    private RadioGroup radioGroupPayment;
+    private RadioButton checkBill;
+    private RadioButton checkCreditCard;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_payment, container, false);
+        View view = inflater.inflate(R.layout.fragment_payment, container, false);
+
+        RadioGroup radioGroupPayment = view.findViewById(R.id.radioGroupPayment);
+        final ImageView imgCreditCard = view.findViewById(R.id.imgCreditCard);
+
+        RadioButton checkBill = view.findViewById(R.id.checkBill);
+        final RadioButton checkCreditCard = view.findViewById(R.id.checkCreditCard);
+        checkCreditCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkCreditCard.isChecked()){
+                    imgCreditCard.setVisibility(View.VISIBLE);
+                }else{
+                    imgCreditCard.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        Button btnFinalize = view.findViewById(R.id.btnFinalize);
+        btnFinalize.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FinalizedFragment finalizedFragment = new FinalizedFragment();
+                getFragmentManager().beginTransaction().replace(R.id.fragmentContainer, finalizedFragment).commit();
+            }
+        });
+
+        return view;
     }
 
 }
+
