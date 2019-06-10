b7773162533614a9d46770e18a7576d019546ff6
Rodrigo Antunes <ro_sgca@hotmail.com>
2019-06-10 01:12:23 -0300

    - Feature Searchbar  add searching

--- app/src/main/java/com/example/epetcommerce/ProductListFragment.java
+++ app/src/main/java/com/example/epetcommerce/ProductListFragment.java
@@ -2,16 +2,15 @@ package com.example.epetcommerce;
 
 
 import android.app.AlertDialog;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.ProgressBar;
+import android.widget.SearchView;
 
 import com.example.epetcommerce.clients.IProductClient;
 import com.example.epetcommerce.database.ProductData;
@@ -38,6 +37,8 @@ public class ProductListFragment extends Fragment {
     RecyclerView cardContainer;
     ProductAdapter adapter;
     RecyclerView.LayoutManager layoutManager;
+    SearchView searchProduct;
+    private ProgressBar progressBarProductList;
 
     public ProductListFragment() {
         // Required empty public constructor
@@ -50,19 +51,45 @@ public class ProductListFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_product_list, container, false);
 
+        progressBarProductList = view.findViewById(R.id.progressBarProductList);
         cardContainer = (RecyclerView) view.findViewById(R.id.cardContainer);
         cardContainer.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(NavigationDrawerActivity.getContext());
         cardContainer.setLayoutManager(layoutManager);
         adapter = new ProductAdapter(new ArrayList<ProductCard>());
         cardContainer.setAdapter(adapter);
-        CarregarLista();
+        CarregarLista("");
+
+
+        //progressBarProductList.setVisibility(View.GONE);
+        searchProduct = view.findViewById(R.id.searchProduct);
+        searchProduct.setQueryHint("O que procura?");
+        searchProduct.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //Log.e("onQueryTextChange", "called");
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                CarregarLista(query);
+
+                // Do your task here
+
+                return false;
+            }
+
+        });
+
         return view;
     }
 
 
-    private void CarregarLista() {
+    private void CarregarLista(String search) {
 
+        progressBarProductList.setVisibility(View.VISIBLE);
 
         Retrofit instanciaRetrofit = new Retrofit.Builder()
                 .baseUrl("https://oficinacordova.azurewebsites.net")
@@ -72,46 +99,56 @@ public class ProductListFragment extends Fragment {
         IProductClient api =
                 instanciaRetrofit.create(IProductClient.class);
 
-        Call<List<Product>> chamada = api.GetProduto();
 
-        Callback<List<Product>> Callback = new Callback<List<Product>>() {
-            @Override
-            public void onResponse(Call<List<Product>> call, Response<List<Product>> response) {
+        Call<List<Product>> chamada;
 
-                List<Product> Products = response.body();
-                final ArrayList<ProductCard> productCards = new ArrayList<ProductCard>();
+        if(search.equals("")){
+            chamada = api.GetProduto();
+        }
+        else{
+            chamada = api.GetSearchProduto(search);
+        }
 
-                if (response.isSuccessful() && Products != null) {
+            Callback<List<Product>> Callback = new Callback<List<Product>>() {
+                @Override
+                public void onResponse(Call<List<Product>> call, Response<List<Product>> response) {
 
-                    for (Product Product : Products) {
-                        //CriarCard(Product, imgUrl);
-                        productCards.add(new ProductCard(Product));
-                    }
+                    List<Product> Products = response.body();
+                    final ArrayList<ProductCard> productCards = new ArrayList<ProductCard>();
 
-                    adapter = new ProductAdapter(productCards);
-                    adapter.setOnItemClickListener(new ProductAdapter.IOnItemClickListener() {
-                        @Override
-                        public void onItemClick(ProductCard ProductCard) {
-                            ProductData productData = ProductData.getInstance();
-                            productData.setActiveProduct(ProductCard.GetProduto());
+                    if (response.isSuccessful() && Products != null) {
 
-                            ShowProductFragment fragProduct = new ShowProductFragment();
-                            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, fragProduct).commit();
-
+                        for (Product Product : Products) {
+                            //CriarCard(Product, imgUrl);
+                            productCards.add(new ProductCard(Product));
                         }
-                    });
-                    cardContainer.setAdapter(adapter);
+
+                        adapter = new ProductAdapter(productCards);
+                        adapter.setOnItemClickListener(new ProductAdapter.IOnItemClickListener() {
+                            @Override
+                            public void onItemClick(ProductCard ProductCard) {
+                                ProductData productData = ProductData.getInstance();
+                                productData.setActiveProduct(ProductCard.GetProduto());
+
+                                ShowProductFragment fragProduct = new ShowProductFragment();
+                                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, fragProduct).commit();
+
+                            }
+                        });
+                        cardContainer.setAdapter(adapter);
+                        progressBarProductList.setVisibility(View.GONE);
+                    }
                 }
-            }
 
-            @Override
-            public void onFailure(Call<List<Product>> call, Throwable t) {
+                @Override
+                public void onFailure(Call<List<Product>> call, Throwable t) {
 
-                String teste = t.getMessage();
+                    String teste = t.getMessage();
 
+                    progressBarProductList.setVisibility(View.GONE);
+                }
+            };
 
-            }
-        };
 
         chamada.enqueue(Callback);
 
--- app/src/main/java/com/example/epetcommerce/clients/IProductClient.java
+++ app/src/main/java/com/example/epetcommerce/clients/IProductClient.java
@@ -6,9 +6,13 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface IProductClient {
 
     @GET("/android/rest/produto")
     Call<List<Product>> GetProduto();
+
+    @GET("/android/rest/produto/{product}")
+    Call<List<Product>> GetSearchProduto(@Path("product") String productName);
 }
